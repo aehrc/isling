f399c92476423d87b045f999294de639e6164ed5
Scott, Suzanne (H&B, North Ryde) <Suzanne.Scott@csiro.au>
2019-07-09 16:01:23 +1000

    pulled latest version of R scripts from cluster with minor bug fixes

--- count_rearrange.R
+++ count_rearrange.R
@@ -2,7 +2,32 @@
 library(tidyverse)
 
 #import data
-data <- read_tsv(file = "test_data/m366T.rearrange.txt") 
+
+data_path = "../data/"
+out_path = "../out/"
+files <- list.files(path = data_path, recursive = TRUE, pattern = "rearrange.txt")
+
+#remove empty files
+isnotempty <- map(files, ~file.info(paste(data_path, ., sep=''))$size != 0) %>% unlist()
+files <- files[isnotempty]
+
+
+#import all datasets
+data <- data_frame(filename = files) %>% # create a data frame holding the file names
+  mutate(integrations = map(filename, ~ read_tsv(file.path(data_path, .), 
+                                                 na = c("", "NA", "?"), 
+                                                 col_types = cols(Chr = col_character(), .default =col_guess())))) %>% 
+  unnest()
+
+
+#add extra columns with sample name, dataset, host
+data <- data %>% 
+  mutate(dataset = dirname(dirname(filename))) %>% 
+  mutate(sample = str_extract(basename(filename), "^[\\w]+(?=\\.)")) %>% 
+  mutate( host = ifelse(str_detect(basename(filename), "mouse|mm10"), "mm10", "hg38"))
+
+
+
 
 #make summary table of number of vector rearrangements
 data %>% 
@@ -22,25 +47,27 @@ for (i in colns) {
     coord_polar("y", start=0) +
     theme_void() +
     theme(legend.position = "bottom")
-  Sys.sleep(3)
+  ggsave(paste0(out_path, "rearrange_",gsub(" ", "", i), ".pdf", sep = ""))
+
 } #end of loop over columns
   
-  
-#compare to FRG vec rearranges
-
-soft <- read_tsv(file = "test_data/FRG206.soft.txt") 
-count(soft, PossibleVectorRearrangement) %>% 
-  mutate( freq = n/sum(n) ) %>% 
-  ggplot(aes(x="", y=freq, fill=PossibleVectorRearrangement)) +
-  geom_bar(stat = "identity", width=1)+
-  geom_label(aes(label = paste0(n, "\n", round(freq*100), "%")), 
-             position = position_fill(vjust = 0.5),
-             show.legend = FALSE) +
-  coord_polar("y", start=0) +
-  theme_void() +
-  theme(legend.position = "bottom")
 
 
-#make pie chart of number of gaps
+#make location histogram for viral rearrangements
+#load bed file
+bedfiles <- list.files(path = data_path, recursive = TRUE, pattern = "rearrange.bed")
+
+#remove empty files
+isnotempty <- map(bedfiles, ~file.info(paste(data_path, ., sep=''))$size != 0) %>% unlist()
+bedfiles <- bedfiles[isnotempty]
+
+
+#import all datasets
+data <- data_frame(filename = bedfiles) %>% # create a data frame holding the file names
+  mutate(integrations = map(filename, ~ read_tsv(file.path(data_path, .), 
+                                                 na = c("", "NA", "?"), 
+                                                 col_types = cols(Chr = col_character(), .default =col_guess())))) %>% 
+  unnest()
+
 
   
