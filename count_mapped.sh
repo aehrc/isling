eefb58adfc604df17c1cf4c444786da25ba9e0a7
Scott, Suzanne (H&B, North Ryde) <Suzanne.Scott@csiro.au>
2019-07-14 15:26:19 +1000

    updated bash script

--- count_mapped.sh
+++ count_mapped.sh
@@ -9,14 +9,14 @@ conda activate integration
 PROJ="`pwd`/.."  #NOTE NEED TO RUN FROM scripts DIRECTORY IN THE CORRECT PROJECT!
 DATA=${PROJ}/out
 
-echo "sample\tdataset\tmapped\tunmapped" > ${PROJ}/out/count_mapped.txt
+printf "sample\tdataset\tmapped\tunmapped\n" > ${PROJ}/out/count_mapped.txt
 files=($(find ${DATA}/*/*/*bam -maxdepth 0 -type f))
 for file in "${files[@]}"; do
 	DATASET=`dirname $file | xargs dirname | xargs basename`
 	SAMPLE=`basename $file`
 	MAPPED=`samtools view -c -F 4 -F 2048 $file` #not unmapped (-F 4), not supplementary (-F 2048)
 	UNMAPPED=`samtools view -c -f 4 $file` #is unmapped (-f 4)
-	printf "%s\t%s\t%s\t%s" $SAMPLE $DATASET $MAPPED $UNMAPPED >> ${PROJ}/out/count_mapped.txt
+	printf "%s\t%s\t%s\t%s\n" $SAMPLE $DATASET $MAPPED $UNMAPPED >> ${PROJ}/out/count_mapped.txt
 done
 
 
