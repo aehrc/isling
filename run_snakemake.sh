652cea8cde9a926caddc49f5625a1449db53390e
Suzanne Scott <suzanne.scott@csiro.au>
2019-07-16 16:45:35 +1000

    modifed to work for SRA data

--- run_snakemake.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source ~/.bashrc
-conda activate snakemake
-
-snakemake -j 100 -s viralIntegrations.sf --cluster-config cluster.json  --rerun-incomplete --cluster "sbatch --mem-per-cpu {cluster.mem-per-cpu}  -t {cluster.time} --mail-user {cluster.mail-user} --nodes {cluster.nodes} --cpus-per-task {cluster.cpus-per-task} --output {cluster.output}"
