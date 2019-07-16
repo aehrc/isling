c88555aeaee2db7877b74c80b83663f8b7fce774
Suzanne Scott <suzanne.scott@csiro.au>
2019-07-16 18:56:17 +1000

    updated wrapper script

--- run_ints.sh
+++ run_ints.sh
@@ -3,4 +3,4 @@
 source ~/.bashrc
 conda activate snakemake
 
-snakemake -j 100 -s viralIntegrations.sf --cluster-config cluster.json  --rerun-incomplete  --use-conda --cluster "sbatch --mem-per-cpu {cluster.mem-per-cpu}  -t {cluster.time} --mail-user {cluster.mail-user} --nodes {cluster.nodes} --cpus-per-task {cluster.cpus-per-task} --output {cluster.output}"
+snakemake -j 100 -s viralIntegrations.sf --cluster-config cluster.json --rerun-incomplete  --use-conda --cluster "sbatch --mem-per-cpu {cluster.mem-per-cpu}  -t {cluster.time} --mail-user {cluster.mail-user} --nodes {cluster.nodes} --cpus-per-task {cluster.cpus-per-task} --output {cluster.output}"
