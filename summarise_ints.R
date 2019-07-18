076320a9ed64b6c7f1f27da61d4858b61c188b06
Suzanne Scott <suzanne.scott@csiro.au>
2019-07-18 10:08:29 +1000

    modified for SRA dataset

--- summarise_ints.R
+++ summarise_ints.R
@@ -24,7 +24,8 @@ files <- files[isnotempty]
 #import all datasets
 data <- data_frame(filename = files) %>% # create a data frame holding the file names
   mutate(integrations = map(filename, ~ read_tsv(file.path(data_path, .), 
-						na = c("", "NA", "?"), 
+						na = c("", "NA", "?"),
+						skip = 1, 
 						col_types = cols(Chr = col_character(), NoAmbiguousBases = col_integer(), .default =col_guess())))) %>% 
   unnest()
 
