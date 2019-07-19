402a17d9fdd099fba4a45a5330cc9b274f7dabd0
Suzanne Scott <suzanne.scott@csiro.au>
2019-07-19 16:19:43 +1000

    added conda to path:

--- run_ints.sh
+++ run_ints.sh
@@ -2,5 +2,6 @@
 
 source ~/.bashrc
 conda activate snakemake
+export PATH="/scratch1/sco305/conda/miniconda/bin:$PATH"
 
 snakemake -j 100 -s viralIntegrations.sf --cluster-config cluster.json --rerun-incomplete  --use-conda --cluster "sbatch --mem-per-cpu {cluster.mem-per-cpu}  -t {cluster.time} --mail-user {cluster.mail-user} --nodes {cluster.nodes} --cpus-per-task {cluster.cpus-per-task} --output {cluster.output}"
