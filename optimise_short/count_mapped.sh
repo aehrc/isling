821d1bf35271c2dd5a331bbed9cb99fba448b376
Suzanne Scott <suzanne.scott@csiro.au>
2019-08-09 15:51:48 +1000

    modied for this folder

---
+++ optimise_short/count_mapped.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#count mapped and unmapped reads using samtools
+
+
+PROJ="`pwd`/.."  #NOTE NEED TO RUN FROM scripts DIRECTORY IN THE CORRECT PROJECT!
+DATA=${PROJ}/out
+
+printf "sample\tdataset\tmapped\tunmapped\n" > ${PROJ}/out/summary/count_mapped.txt
+files=($(find ${DATA}/*/*/*bam -maxdepth 0 -type f))
+for file in "${files[@]}"; do
+	DATASET=`dirname $file | xargs dirname | xargs basename`
+	SAMPLE=`basename $file`
+	MAPPED=`samtools view -c -F 4 -F 2048 $file` #not unmapped (-F 4), not supplementary (-F 2048)
+	UNMAPPED=`samtools view -c -f 4 $file` #is unmapped (-f 4)
+	printf "%s\t%s\t%s\t%s\n" $SAMPLE $DATASET $MAPPED $UNMAPPED >> ${PROJ}/out/summary/count_mapped.txt
+done
+
+
+
