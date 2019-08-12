cfad2399fc97a86e92b0a719f16478caa4bf157b
Suzanne Scott <suzanne.scott@csiro.au>
2019-08-12 11:46:40 +1000

    fixed output for samtools calls

--- optimise_short/count_mapped.sh
+++ optimise_short/count_mapped.sh
@@ -5,15 +5,16 @@
 
 PROJ="`pwd`/../.."  #NOTE NEED TO RUN FROM scripts DIRECTORY IN THE CORRECT PROJECT!
 DATA=${PROJ}/out
+OUT=${PROJ}/out/summary/short_test/count_mapped.txt
 
-printf "sample\tdataset\tmapped\tunmapped\n" > ${PROJ}/out/summary/short_test/count_mapped.txt
+printf "sample\tdataset\tmapped\tunmapped\n" > ${OUT}
 files=($(find ${DATA}/*/*/*bam -maxdepth 0 -type f))
 for file in "${files[@]}"; do
 	DATASET=`dirname $file | xargs dirname | xargs basename`
 	SAMPLE=`basename $file`
 	MAPPED=`samtools view -c -F 4 -F 2048 $file` #not unmapped (-F 4), not supplementary (-F 2048)
 	UNMAPPED=`samtools view -c -f 4 $file` #is unmapped (-f 4)
-	printf "%s\t%s\t%s\t%s\n" $SAMPLE $DATASET $MAPPED $UNMAPPED >> ${PROJ}/out/summary/count_short_mapped.txt
+	printf "%s\t%s\t%s\t%s\n" $SAMPLE $DATASET $MAPPED $UNMAPPED >> ${OUT}
 done
 
 
