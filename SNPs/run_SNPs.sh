6649052f85bf3cb61a8c7e94d23780cd69696b9f
Suzanne Scott <suzanne.scott@csiro.au>
2019-10-25 15:11:57 +1100

    changed calling to bcftools

--- SNPs/run_SNPs.sh
+++ SNPs/run_SNPs.sh
@@ -4,4 +4,4 @@ source ~/.bashrc
 conda activate snakemake
 export PATH="/scratch1/sco305/conda/miniconda/bin:$PATH"
 
-snakemake -j 100 -s SNPs.sf --cluster-config cluster.json --rerun-incomplete --use-conda --cluster "sbatch --mem-per-cpu {cluster.mem-per-cpu}  -t {cluster.time} --mail-user {cluster.mail-user} --nodes {cluster.nodes} --cpus-per-task {cluster.cpus-per-task} --output {cluster.output}"
+snakemake -s SNPs.sf -j 100 --cluster-config ./cluster.json --rerun-incomplete --use-conda --cluster "sbatch --mem-per-cpu {cluster.mem-per-cpu}  -t {cluster.time} --mail-user {cluster.mail-user} --nodes {cluster.nodes} --cpus-per-task {cluster.cpus-per-task} --output {cluster.output}"
