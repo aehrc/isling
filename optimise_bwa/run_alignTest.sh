0c88512978a8c0a31c18574cbf1b5e0f9ff4fcab
Suzanne Scott <suzanne.scott@csiro.au>
2019-12-05 14:57:17 +1100

    bug fixes for alignment test

--- optimise_bwa/run_alignTest.sh
+++ optimise_bwa/run_alignTest.sh
@@ -4,4 +4,4 @@ source ~/.bashrc
 conda activate snakemake
 export PATH="/scratch1/sco305/conda/bin:$PATH"
 
-snakemake -j 100 -s testAlign.sf --cluster-config cluster.json --rerun-incomplete --use-conda --cluster "sbatch --mem-per-cpu {cluster.mem-per-cpu}  -t {cluster.time} --mail-user {cluster.mail-user} --nodes {cluster.nodes} --cpus-per-task {cluster.cpus-per-task} --output {cluster.output}"
+snakemake -j 100 -s testAlign.sf --cluster-config cluster.json --rerun-incomplete --use-conda --cluster "sbatch --mem-per-cpu {cluster.mem-per-cpu}  -t {cluster.time} --nodes {cluster.nodes} --cpus-per-task {cluster.cpus-per-task} --output {cluster.output}"
