920b23ea475296401922494282aef98e086ff711
Susie Grigson <Susie.Grigson@csiro.au>
2020-01-03 17:01:12 +1100

    changes made to look at the rearrangements and deletions and to create a dataframe of the false reads

--- benchmarking/pipeline_success.py
+++ benchmarking/pipeline_success.py
@@ -42,7 +42,7 @@ def main(argv):
 	filt_pipe = filterAmbiguous(pipe_ints)
  
 	true_vreads, pred_vreads = listIDs(read_ints,filt_pipe)
-	correct_reads, missed_reads = listSuccess(true_vreads, pred_vreads)
+	correct_reads, missed_reads, false_reads = listSuccess(true_vreads, pred_vreads)
 	print("Stats after filtering...")
 	getStats(pred_vreads,true_vreads,correct_reads)
 
@@ -51,35 +51,32 @@ def main(argv):
 	missed_hPos = findMissed(read_ints, correct_reads)
 	evaluateMissed(missed_hPos,ints)
 
-	#analyse length of missed reads 
+	#analyse length of missed reads -this is potentially bad - remove later
 	assessLength(read_ints, missed_reads)
 
-	#get the reads that we correctly identified so that we can look for commonalities
-	#missed_df = readDataFrame(correct_reads, read_ints) 
+	#get the reads that we correctly identified
+	missed_df = readDataFrame(correct_reads, read_ints) 
 	 
 	assessMissedLength(missed_hPos, ints)
 
+	read_ints.to_csv("filtered_set.csv",sep = '\t') 
+
+	false_df = pipelineDataFrame(false_reads,pipe_ints) 
+	false_df.to_csv("false_reads.csv",sep='\t') 
 	#get the actual types of junctions 
 	#assessOverlap(read_ints, missed_reads)  
-
-	#print("columns in pipeline file" +pipe_ints.columns.to_series()) 
-	#print("columns in reads file" +read_ints.columns.to_series()) 
-	#compare the number of entries in each of the files 
-	#print("Number of entries in ints file: "+str(len(ints)))
-	#print("Number of entries in host_ints file: "+str(len(host_ints)))
-	#print("Number of entries in read_ints file: "+str(len(read_ints)))
-	#print("Number of entries in pipe_ints file: "+str(len(pipe_ints)))
 
 	
 def listIDs(read_ints,pipe_ints): 
 	"""Function which makes lists of the IDs of the viral reads and the predicted viral reads""" 
-	#create a list of the IDs of the known viral reads 	
+	#create a list of the IDS of all viral reads 	
 	true_vreads = []
 	for i in range(len(read_ints)):
 		if read_ints.loc[i]['fragment_id'] not in true_vreads:  
-			true_vreads.append(read_ints.loc[i]['fragment_id'])  
+			true_vreads.append(read_ints.loc[i]['fragment_id'])
+		 
 		
-	#create a list of the IDS of the predicted viral reads 
+	#create a list of the IDS of the reads detected by the pipeline  
 	pred_vreads = []
 	for i in range(len(pipe_ints)):
 		if pipe_ints.loc[i]['ReadID'] not in pred_vreads:  
@@ -91,7 +88,8 @@ def listSuccess(true_vreads, pred_vreads):
 	"""function which creates a list of the IDs successfully predicted by the pipeline and those missed""" 
 
 	correct_reads = [] #list of reads which were accurately predicted 
-	missed_reads = [] #list of reads containing viral DNA missed by the pipeline 
+	missed_reads = [] #list of reads containing viral DNA missed by the pipeline
+	false_reads = [] #list of reads falsely predicted to contain viral DNA  
 
 	for int in true_vreads: 
 		if int in pred_vreads:
@@ -99,7 +97,11 @@ def listSuccess(true_vreads, pred_vreads):
 		else: 
 			missed_reads.append(int) 
 
-	return correct_reads, missed_reads
+	for int in pred_vreads: 
+		if int not in true_vreads: 
+			false_reads.append(int)
+		
+	return correct_reads, missed_reads, false_reads
 
 def getStats(pred_vreads,true_vreads,correct_reads): 
 	"""Function which identifies the accuracy which reads containing viral DNA are identified""" 
@@ -229,6 +231,20 @@ def readDataFrame(read_list, read_ints):
 
 	return new_df
 
+def pipelineDataFrame(read_list, read_ints): 
+	"""Creates a dataframe of a subset of  reads using a list of read IDs""" 
+	
+	read_ints = read_ints.set_index('ReadID') 
+	
+	#create df with only the entries in read_list
+	new_df = read_ints.loc[read_list]
+
+	#reset index on the new dataframe 
+	new_df['ReadID'] = new_df.index 
+	new_df = new_df.reset_index(drop=True)
+
+	return new_df
+
 
 def findMissed(read_ints, correct_reads): 
 
@@ -262,7 +278,7 @@ def findMissed(read_ints, correct_reads):
 			if value not in pipeline_hPos: 
 				pipeline_hPos.append(value) 
 
-	print("Number of integrations detectable from the pipeline: "+str(len(pipeline_hPos))) 
+	print("Number of integrations detected by the pipeline: "+str(len(pipeline_hPos))) 
 
 	acc = (len(pipeline_hPos)/len(detectable_hPos))*100
 
@@ -279,16 +295,28 @@ def evaluateMissed(missed_hPos,ints):
 	"""Attempt to find out why the integrations were completely missed by the pipeline""" 
 	#create a dictionary of properities of each of the integrations
 	hPos_dict = ints.set_index('hPos').to_dict()
- 
+	hPos_keys = hPos_dict.get('rearrangement').keys()
+
 	#check out the different properities to see if we can find any particular issues 
-	rearrange = [hPos_dict.get('rearrangement').get(missed) for missed in missed_hPos]
-	deletion = [hPos_dict.get('deletion').get(missed) for missed in missed_hPos]
-	num_fragments = [hPos_dict.get('num_fragments').get(missed) for missed in missed_hPos]
+	miss_rearrange = [hPos_dict.get('rearrangement').get(missed) for missed in missed_hPos]
+	all_rearrange = [hPos_dict.get('rearrangement').get(key) for key in hPos_keys]
+	miss_deletion = [hPos_dict.get('deletion').get(missed) for missed in missed_hPos]
+	all_deletion = [hPos_dict.get('deletion').get(key) for key in hPos_keys]
+	miss_num_fragments = [hPos_dict.get('num_fragments').get(missed) for missed in missed_hPos]
+	all_num_fragments = [hPos_dict.get('num_fragments').get(key) for key in hPos_keys]
 
 	#get statistics on the different properties 
-	assessRearrange(rearrange) 
-	assessDeletion(deletion)
-	assessFragments(num_fragments)  #TODO compare these to total amount which were deletions etc. 
+	#get statisitics on rearangements 
+	print("\n"+str(percentTrue(all_rearrange))+"% of the total integrations were rearranged") 
+	print(str(percentTrue(miss_rearrange))+"% of the integrations not detected by the pipeline were rearraged")
+	#get statistics on deletions 
+	print("\n"+str(percentTrue(all_deletion))+"% of the total integrations had deletions")
+	print(str(percentTrue(miss_deletion))+"% of the integrations not detected by the pipeline had deletions\n")
+	#get the statistics on the number of fragments use to separate the fragments 
+	print("For all of the integrations...") 
+	assessFragments(all_num_fragments)
+	print("\nFor integrations missed by the pipeline...") 
+	assessFragments(miss_num_fragments)   #TODO compare these to total amount which were deletions etc. 
 
 def assessMissedLength(missed_hPos, ints): 
 	"""Look at the properities of the integrations which were not detected by the pipeline""" 
@@ -325,25 +353,15 @@ def assessMissedLength(missed_hPos, ints):
 	plt.pyplot.savefig("missed_integration_lengths.pdf")
 	print("Distribution of all integrations saved to missed_integration_lengths.pdf")
 	
-def assessRearrange(rearrange): 
-	"""Tells us how likely it is that rearrangees are the cause of misses""" 
+def percentTrue(values): 
+	"""counts the number of true variables in a list of values""" 
 
 	#find the number of missed integrations which have been rearranged 
-	rearr_true = rearrange.count(True) 
+	count  = values.count(True) 
 
-	missed_rearr = (rearr_true/len(rearrange))*100 
-	
-	print("\n" + str(missed_rearr)+"% of the integrations missed were rearraged\n")   
+	per_true = (count/len(values))*100 
 
-def assessDeletion(deletion): 
-	"""Tells us how likely it is that deletions ar ethe cause of misses""" 
-	
-	#find the number of missed integrations which had deletions
-	del_true = deletion.count(True) 
-
-	missed_del = (del_true/len(deletion))*100
-
-	print(str(missed_del)+"% of the integrations missed had deletions\n")
+	return per_true
 
 def assessFragments(num_fragments): 
 	"""Tells us what number of fragments the missed integrations were divided into""" 
@@ -354,7 +372,7 @@ def assessFragments(num_fragments):
 	for i in range(min_frag, max_frag+1):
 		count = num_fragments.count(i)
 		frag_freq = (count/len(num_fragments))*100 
-		print(str(frag_freq)+"% of missed integrations were broken into "+str(i)+" fragments")
+		print(str(frag_freq)+"% of integrations were broken into "+str(i)+" fragments")
 
 
 def getOverlap(read_ints):
@@ -369,6 +387,7 @@ Inormation corresponds to read ID"""
 
 	#column which says the type of overlaps 
 	overlap_type = []
+	
 
 	for i in range(len(read_ints)):
 		if left_read[i] == "viral" and right_read[i] == 'host' or right_read[i] == 'viral' and left_read[i] == 'host': 
@@ -387,7 +406,9 @@ Inormation corresponds to read ID"""
 	#make into a dictionary - easy to make comparison 
 	overlap_dict = dict(zip(int_ID, overlap_type)) 
 
-	return overlap_dict 
+	return overlap_dict
+
+	
 	
 def assessLength(read_ints, missed_reads): #TODO this is questionable remove 
 	"""Compare the number of viral base pairs in the missed reads and in alfg
@@ -397,7 +418,7 @@ l reads"""
 	first_len = read_ints['left_read_amount'].values
 	second_len = read_ints['right_read_amount'].values
 
-	#find the length of virus in each read - excluding reads consisting of whole virus and no virus 
+	#find the length of virus in each read - excluding reads consisting of whole virus and no virus
 	viral_len = [max(first_len[i],second_len[i]) for i in range(len(read_ints))]	
 	
 	#create a dictionay
@@ -411,10 +432,13 @@ l reads"""
 	ax.set(xlabel='length of missed integration', ylabel = 'Density')  
 	fig = ax.get_figure()
 	fig.savefig("missedread_len_distplot.pdf")
-	print("Distribution of the length of the missed reads saved to missedread_len_distplot.pdf") 
+	print("Distribution of the length of the missed reads saved to missedread_len_distplot.pdf")
+
 
 	
-#def assessIncorrectPredictions #TODO look at the integrations which were falsely detected by the pipeline 
+#def assessFalseReads #TODO look at the integrations which were falsely detected by the pipeline 
+#look at the junctions in them 
+#were they close to an actual integration event? 
 
 def assessOverlap(read_ints, missed_reads): 
 	"""Looks at what type of overlap the reads missed by the pipeline have. Takes a dataframe of all of the reads and a list of the IDs of the missed reads""" 
@@ -441,14 +465,14 @@ def assessOverlap(read_ints, missed_reads):
 	print(str(otherFreq)+"%of missed reads had a junction other than those above") 
 	
 
-def compareOverlap(read_ints, pipe_ints): 
+def compareOverlap(read_ints, pipe_ints): #TODO finish this 
 	"""Compares whether the pipeline has correctly predicted the gap type of the read. Takes list of predicted"""
 
 	#get the IDs of the pipeline reads
 	pipe_reads = pipe_ints['ReadID'].values 
 
 	#get the overlap type of the pipeline reads
-	pipe_overlap = pip_ints['Type'].values
+	pipe_overlap = pipe_ints['Type'].values
  
 	#create a dictionary of the overlap types for each read 
 	overlap_dict = getOverlap(read_ints)
@@ -468,7 +492,7 @@ def compareOverlap(read_ints, pipe_ints):
 	print("Pipeline predicted the type of junction correctly "+str(accuracy)+"% of the time:") 
 
 	#we also want to know which ones we got wrong 
-	#find the distribtuion of rearrangements and deletions so that these can be compared - plot using seaborn? 
+	#find the distribtuion of rearrangements and deletions so that these can be compared - plot using seaborn?
 
 
 if __name__ == "__main__":
