5a99edc7f3bbc09e002dede4a40ea0140d5093b9
Susie Grigson <Susie.Grigson@csiro.au>
2020-02-11 15:30:41 +1100

    debugged and made changes from errors indentified in testing

--- benchmarking/read_host_location.py
+++ benchmarking/read_host_location.py
@@ -14,50 +14,56 @@ def main(argv):
 
 	#get arguments
 	parser = argparse.ArgumentParser(description='the location of reads in the host alignment')
-	parser.add_argument('--locs', help = 'text file containing details of the integrations', required = True)
 	parser.add_argument('--reads', help = 'csv containing information the integrations in reads', required = True)
+	parser.add_argument('--host_ints', help = 'file containing locations of host integrations', required = True) 
+	args = parser.parse_args()
 
-	#make dataframe from locs file 
-	locs = pd.read_csv(args.locs, header =0, sep='\t')
 
 	#make dataframe from host ints file
 	reads = pd.read_csv(args.reads, header = 0 , sep='\t') 
 
-	
-	#make a dictionary of the length of each integration 
-	int_lens = [locs[vStop][i]-locs[vStart][i] for i in range(len(locs))]
-	int_dict = pd.Series(int_lens,index = locs.vStart)
+	#read in the host ints file 
+	host_ints = pd.read_csv(args.host_ints, header = 0 , sep='\t') 	
 
-	
+	#make a dictionary of the length of each integration 
+	int_lens = [host_ints['Stop point'][i]-host_ints['Start point'][i]+1 for i in range(len(host_ints))]
+	int_dict = pd.Series(int_lens,index = host_ints['Start point'])
+
 	#adjust right reads
-	adj_Rread = []
+	adj_Rreads = []
+
 
 	for i in range(len(reads)): 
-		if reads["right_read"]!= 'v': 
-			start, stop = read['right_read_coor'][i]
+		if reads["right_read"][i] != 'v': 
+			right_coor = reads['right_read_coor'][i].split(", ")
+			start = int(right_coor[0].replace('(','')) 
+			stop = int(right_coor[1].replace(')','')) 
 		else: 
 			start, stop = (-1,-1) 
  
-		for ints in locs['vStart']: 
+		for ints in host_ints['Start point']:
 			if ints < start: 
 				start = start - int_dict.get(ints)
 				stop = stop - int_dict.get(ints)
 		adj_Rreads.append((start,stop)) 
 
 	
-	#adjust right reads 
-	adj_Lread = [] 
+	#adjust left reads 
+	adj_Lreads = []
+
 	for i in range(len(reads)): 
-		if reads["left_read"]!= 'v': 
-			start, stop = read['left_read_coor'][i]
+		if reads["left_read"][i] != 'v': 
+			left_coor = reads['left_read_coor'][i].split(", ")
+			start = int(left_coor[0].replace('(','')) 
+			stop = int(left_coor[1].replace(')','')) 
 		else: 
 			start, stop = (-1,-1) 
  
-		for ints in locs['vStart']: 
+		for ints in host_ints['Start point']:
 			if ints < start: 
 				start = start - int_dict.get(ints)
 				stop = stop - int_dict.get(ints)
-		adj_Lreads.append((start,stop))
+		adj_Lreads.append((start,stop)) 
 
 	
 	#save the adjusted coordinates as a dataframe 
@@ -65,8 +71,9 @@ def main(argv):
 	reads["host_right"] = pd.Series(adj_Rreads,index = reads.index) 
 
 	#save to csv file 
-	reads.to_csv('adjusted_reads.csv', sep = '\t') 
+	reads.to_csv('host_location_reads.csv', sep = '\t') 
 
+	print("COMPLETE") 
 
 if __name__ == "__main__":
 	main(sys.argv[1:]) 
