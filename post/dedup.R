ea3555038666de44790001041bf790a141382e3a
Scott, Suzanne (H&B, North Ryde) <Suzanne.Scott@csiro.au>
2020-03-20 14:21:39 +1100

    added counts of duplicated reads to post/dedup.R

--- post/dedup.R
+++ post/dedup.R
@@ -3,7 +3,17 @@
 # reatain only integrations with distinct read sequences
 # consider only exact duplicates
 
+# get counts of each read
+counts <- ints %>% 
+  dplyr::count(merged) %>% 
+  dplyr::rename(n_duplicates = n)
+
+# retain only distinct rows
 ints <- ints %>% 
   dplyr::distinct(merged, .keep_all = TRUE)
+
+# join counts info with ints
+ints <- ints %>% 
+  dplyr::left_join(counts, by="merged")
 
 cat(nrow(ints), "integrations remaining after de-duplication based on read sequences\n")
--- post/test_post_local.R
+++ post/test_post_local.R
@@ -2,7 +2,7 @@
 #run from directory from below 'post'
 # ie Rscript post/test_postprocess.R
 
-call <- "Rscript post/postprocess.R ../out/FRG_OTC/ints/FRG203_C73YB_CGAACTTA_L001.GRCh38.pAAV2-OTC.integrations.txt"
+call <- "Rscript post/postprocess.R ../out/FRG_OTC/ints/FRG203_C73YB_CGAACTTA.GRCh38.pAAV2-OTC.integrations.txt"
 dedup <- "dedup"
 filter <- "filter"
 mask_exclude <- c("mask-exclude", "../data/references/GRCh38/hg38_homologoustoOTCvec.bed")
