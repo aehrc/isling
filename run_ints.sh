ed714bfbb04e9e2ccc8be5a09c08d77dbe8f4fc7
Suzanne Scott <suzanne.scott@csiro.au>
2020-03-24 17:29:50 +1100

    sometimes the rule 'post' fails, even though running it outside of snakemake works.  I think this is because of the issue described here: https://github.com/conda-forge/r-base-feedstock/issues/67 Basically, every time snakemake activates the conda envirnoment containing R, it updates a file `$CONDA_ENV/lib/R/etc/ldpaths`.  This file is required for activation of the environment, but there is a short amount of time that this file does not exist because it is being updated (and filesystem latency).  So if snakemake tries to activate this environment many times in a short period of time, there is a chance that this file won't be available for one of those activations.  This causes the whole pipeline to crash, because a job failed.  There doesn't seem to be a solution to this problem on the github page linked above.  A temporary workaround is to just try to re-run jobs that failed using the --restart-times flag for snakemake.  But this problem needs a better solution.

--- run_ints.sh
+++ run_ints.sh
@@ -4,4 +4,4 @@ source ~/.bashrc
 eval "$(conda shell.bash hook)"
 conda activate snakemake
 
-snakemake --configfile $1 -j 100 -s viralIntegrations.sf --cluster-config cluster.json --rerun-incomplete --use-conda --cluster "sbatch --mem-per-cpu {cluster.mem-per-cpu}  -t {cluster.time} --mail-user {cluster.mail-user} --nodes {cluster.nodes} --cpus-per-task {cluster.cpus-per-task} --output {cluster.output}" $2
+snakemake --configfile $1 -j 100 --restart-times 3 -s viralIntegrations.sf --cluster-config cluster.json --rerun-incomplete --use-conda --cluster "sbatch --mem-per-cpu {cluster.mem-per-cpu}  -t {cluster.time} --mail-user {cluster.mail-user} --nodes {cluster.nodes} --cpus-per-task {cluster.cpus-per-task} --output {cluster.output}" $2
