828d6f601c6c9ea9e737f02e03a25939750c8e72
Suzanne Scott <suzanne.scott@csiro.au>
2020-04-04 00:38:41 +1100

    use new snakefile

--- run_ints.sh
+++ run_ints.sh
@@ -4,6 +4,5 @@ source ~/.bashrc
 eval "$(conda shell.bash hook)"
 conda activate snakemake
 
-snakemake --configfile $1 -j 100 -s viralIntegrations.sf --rerun-incomplete --cluster-config cluster.json --use-conda --profile slurm "${a[@]:1}"
+snakemake --configfile $1 -j 100 --rerun-incomplete --cluster-config cluster.json --use-conda --profile slurm --dry-run "${a[@]:1}"
 
-# --restart-times 3
