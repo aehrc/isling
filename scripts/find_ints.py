35684a58c9205a10077d6fd083a4e010d05fd84d
Scott, Suzanne (H&B, North Ryde) <Suzanne.Scott@csiro.au>
2021-04-19 13:31:33 +1000

    started python script to find integrations

---
+++ scripts/find_ints.py
@@ -0,0 +1,161 @@
+import argparse
+import pysam
+import pdb
+
+# note python 3.7+ is required
+
+def main():
+	
+	parser = argparse.ArgumentParser(description='Identify integrations by comparing a host and viral alignment')
+	parser.add_argument('--host', help="host alignment bam (query-sorted)", required=True)
+	parser.add_argument('--virus', help="virus alignment bam (query-sorted)", required=True)
+	parser.add_argument('--verbose', '-v', help="print extra messages?", action='store_true')
+	args = parser.parse_args()
+
+	pair = AlignmentPair(args.host, args.virus, args.verbose)
+	
+	for tmp in pair.get_aligns():
+		if pair.curr_host is None or pair.curr_virus is None:
+			break
+		print(f"checking {pair.curr_host[0].qname} and {pair.curr_virus[0].qname}")
+		
+	pair.close()
+	
+		
+class AlignmentPair:
+	""" A class to hold host and viral alignments """
+	
+	def __init__(self, host, virus, verbose):
+		""" Open host and viral alignments, and check for query sorting """
+		
+		self.host = Alignment(host, verbose)
+		self.virus = Alignment(virus, verbose)
+		self.verbose = verbose
+		
+		if self.verbose:
+			print(f"using host bam {host} and virus bam {virus}")
+
+	def get_aligns(self):
+		""" An iterator to get alignments for the same read from both host and virus"""
+
+		# collect alignments from file
+		self.curr_host = self.host.collect_next_read_alignments()
+		self.curr_virus = self.virus.collect_next_read_alignments()
+
+		# continue until we've either found the same read or reached the end of one of the files
+		while True:
+		
+			if self.curr_host is None or self.curr_virus is None:
+				return
+		
+			# check if the query names are the same for host and viral alignments
+			if self.curr_host[0].qname == self.curr_virus[0].qname:
+				yield
+				self.curr_host = self.host.collect_next_read_alignments()
+				self.curr_virus = self.virus.collect_next_read_alignments()
+
+				
+			# host qname less than viral qname
+			elif self.curr_host[0].qname > self.curr_virus[0].qname:
+				if self.verbose:
+					print(f"warning: host bam has no alignment for read {self.curr_virus[0].qname} in virus bam, skipping")
+				self.curr_virus = self.virus.collect_next_read_alignments()
+
+							
+			# viral qname less than host qname
+			elif self.curr_virus[0].qname > self.curr_host[0].qname:
+				if self.verbose:
+					print(f"warning: virus bam has no alignments for read {self.curr_host[0].qname} in host bam, skipping")
+				self.curr_host = self.host.collect_next_read_alignments()
+
+	def close(self):
+		""" Close both alignments """
+
+		self.host.close()
+		self.virus.close()
+		
+
+class Alignment:
+	""" Basically just pysam.AlignmentFile, but with a few extra methods """
+	
+	def __init__(self, bam_path, verbose):
+		""" Open file with pysam, check if query-sorted """
+		
+		# open file
+		self.filename = bam_path
+		self.aln = pysam.AlignmentFile(bam_path,'rb')
+		self.end = False
+		self.verbose = verbose
+
+		# check for query sort 
+		error = f"The bam file {bam_path} does not appear to be query-sorted.  Please sort (i.e. samtools sort -n -o <sorted> <unsorted>) and try again"
+		if 'HD' not in self.aln.header.to_dict():
+			raise ValueError(error)
+		if 'SO' not in self.aln.header.to_dict()['HD']:
+			raise ValueError(error)			
+		if self.aln.header.to_dict()['HD']['SO'] != 'queryname':
+			raise ValueError(error)
+
+		# get first read from file
+		try:
+			self.curr = next(self.aln)
+		except StopIteration:
+			self.end = True
+			self.curr = None
+
+
+	def collect_next_read_alignments(self):
+		""" Collect the alignments (primary, secondary, supplementary) for the next read (pair) in the file """	
+		
+		alns = []
+			
+		qname = self.curr.qname
+		
+		if self.end:
+			return None
+		
+		# keep getting elements until we either reach a different read or the end of the list
+		while (self.curr.qname == qname) and (self.curr.qname is not None):
+			# includes R1, R2, primary, secondary, supplementary alignments
+			alns.append(self.curr)		
+			self._get_next_align()
+			if self.end:
+				break
+				
+		return alns
+
+		
+	def close(self):
+		""" Close alignment file """
+		
+		self.aln.close()
+		
+		if self.verbose:
+			print(f"closing alignment file {self.filename}")
+		
+	def _get_next_align(self):
+		""" Get next alignment from file and assign to self.curr """
+		if not self.end:
+			try:
+				self.curr = next(self.aln)
+			except StopIteration:
+				self.end = True
+				self.curr.qname = None
+				if self.verbose:
+					print(f"reached end of file {self.filename}")
+			
+
+def check_for_int(host, virus):
+	""" Check if the host and viral alignments of a read are complementary """
+	
+	# check that we're dealing with the same read from both alignments
+	assert host.qname == virus.qname
+	assert (host.is_read1 and virus.is_read1) or (host.is_read2 and virus.is_read2)
+	
+	assert (not host.is_secondary) and (not virus.is_secondary)
+	assert (not host.is_supplementary) and (not host.is_supplementary)
+	
+
+
+if __name__ == "__main__":
+	main()
