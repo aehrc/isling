116d1acc5f4f2f037d639149b79969a03830e1dc
Suzanne Scott <suzanne.scott@csiro.au>
2021-08-09 11:15:24 +1000

    removed individual scripts

--- benchmark/sra_data/src/run_isling/run_Nelson_2019_PRJNA485509.sh
+++
@@ -1,8 +0,0 @@
-eval "$(conda shell.bash hook)"
-conda activate snakemake
-
-module load singularity
-
-cd ../../intvi_pipeline
-
-snakemake --keep-going --rerun-incomplete --use-singularity --profile slurm --jobs 100 --configfile ../config/Nelson_2019_PRJNA485509/isling.yml
