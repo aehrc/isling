7ac4ed96f46c8377386b5b0f01fd8d39c68cb1ac
Suzanne Scott <suzanne.scott@csiro.au>
2021-08-09 11:15:24 +1000

    removed individual scripts

--- benchmark/sra_data/src/run_isling/run_Lau_2014_SRP023539.sh
+++
@@ -1,8 +0,0 @@
-eval "$(conda shell.bash hook)"
-conda activate snakemake
-
-cd ../../
-
-snakemake --keep-going --rerun-incomplete --use-singularity --profile slurm --jobs 100 --configfile benchmarking/sra_data/config/isling/Lau_2014_SRP023539_hg19.yml
-
-
