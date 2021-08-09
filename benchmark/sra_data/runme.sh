b17473fc9af1e866b697a0a3c7c50897edb4aeb0
Suzanne Scott <suzanne.scott@csiro.au>
2021-08-09 11:38:30 +1000

    added isling run for benchmarking data

--- benchmark/sra_data/runme.sh
+++ benchmark/sra_data/runme.sh
@@ -42,3 +42,12 @@ bash src/download_sra/run_get_SRA.sh config/download/PRJEB2869.yml
 echo "running isling on PRJNA485509"
 bash src/run_isling/run_isling.sh benchmark/sra_data/config/isling/Nelson_2019_PRJNA485509.yml
 
+# run SRP023539 (Lau)
+echo "running isling on SRP023539"
+bash src/run_isling/run_isling.sh benchmark/sra_data/config/isling/Lau_2014_SRP023539_hg19.yml 
+
+# run PRJEB2869 (Sung)
+echo "running isling on PRJEB2869"
+bash src/run_isling/run_isling.sh benchmark/sra_data/config/isling/Sung_2012_PRJEB2869.yml.yml 
+
+
