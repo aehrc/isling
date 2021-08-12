76145657c01270870fd3eac6a70d537a8264c1aa
Suzanne Scott <suzanne.scott@csiro.au>
2021-08-12 16:02:46 +1000

    inculded option to specify number of cores

--- benchmark/sra_data/src/run_isling/run_isling.sh
+++ benchmark/sra_data/src/run_isling/run_isling.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-# usage: run_isling.sh <CONFIGFILE>
+# usage: run_isling.sh <CONFIGFILE> <CORES>
 
 CONFIG="$1"
+CORES="$2"
 
 cd ../..
 
-snakemake --keep-going --rerun-incomplete --use-singularity --jobs 1 --configfile $CONFIG
+snakemake --keep-going --rerun-incomplete --use-singularity --cores $CORES --configfile $CONFIG
