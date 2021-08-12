93eeefb7f590313176e273971b09fe3022bda2ed
Suzanne Scott <suzanne.scott@csiro.au>
2021-08-12 16:02:46 +1000

    inculded option to specify number of cores

--- benchmark/sra_data/runme.sh
+++ benchmark/sra_data/runme.sh
@@ -7,6 +7,8 @@
 
 set -euo pipefail
 
+CORES="20"
+
 #### dependencies ####
 
 #module load parallel
@@ -28,42 +30,42 @@ bash src/download_references/download_refs.sh
 
 # download for PRJNA485509 (Nelson)
 echo "downloading reads for PRJNA485509"
-bash src/download_sra/run_get_SRA.sh config/download/PRJNA485509.yml
+bash src/download_sra/run_get_SRA.sh config/download/PRJNA485509.yml $CORES
 
 # download for SRP023539" (Lau)
 echo "downloading reads for SRP023539"
-bash src/download_sra/download_SRP023539.sh
+bash src/download_sra/download_SRP023539.sh $CORES
 
 # download for PRJEB2869 (Sung)
 echo "downloading reads for PRJEB2869"
-bash src/download_sra/run_get_SRA.sh config/download/PRJEB2869.yml
+bash src/download_sra/run_get_SRA.sh config/download/PRJEB2869.yml $CORES
 
 #### isling ####
 
 # run PRJNA485509 (Nelson)
 echo "running isling on PRJNA485509"
-bash src/run_isling/run_isling.sh benchmark/sra_data/config/isling/Nelson_2019_PRJNA485509.yml
+bash src/run_isling/run_isling.sh benchmark/sra_data/config/isling/Nelson_2019_PRJNA485509.yml $CORES
 
 # run SRP023539 (Lau)
 echo "running isling on SRP023539"
-bash src/run_isling/run_isling.sh benchmark/sra_data/config/isling/Lau_2014_SRP023539_hg19.yml 
+bash src/run_isling/run_isling.sh benchmark/sra_data/config/isling/Lau_2014_SRP023539_hg19.yml $CORES 
 
 # run PRJEB2869 (Sung)
 echo "running isling on PRJEB2869"
-bash src/run_isling/run_isling.sh benchmark/sra_data/config/isling/Sung_2012_PRJEB2869.yml
+bash src/run_isling/run_isling.sh benchmark/sra_data/config/isling/Sung_2012_PRJEB2869.yml $CORES
 
 #### other tools ####
 
 echo "running other viral integration tools on PRJNA485509"
-bash src/run_other_tools/run_other_tools.sh config/other_tools/Nelson_2019_PRJNA485509.yml
+bash src/run_other_tools/run_other_tools.sh config/other_tools/Nelson_2019_PRJNA485509.yml $CORES
 
 # run SRP023539 (Lau)
 echo "running other viral integration tools on SRP023539"
-bash src/run_other_tools/run_other_tools.sh config/other_tools/Lau_2014_SRP023539_hg19.yml 
+bash src/run_other_tools/run_other_tools.sh config/other_tools/Lau_2014_SRP023539_hg19.yml $CORES
 
 # run SRP023539 (Lau)
 echo "running other viral integration tools on PRJEB2869"
-bash src/run_other_tools/run_other_tools.sh config/other_tools/Sung_2012_PRJEB2869.yml
+bash src/run_other_tools/run_other_tools.sh config/other_tools/Sung_2012_PRJEB2869.yml $CORES
 
 #### generate figures ####
 
--- benchmark/sra_data/src/download_sra/download_SRP023539.sh
+++ benchmark/sra_data/src/download_sra/download_SRP023539.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+CORES=$1
+
 mkdir -p data/reads/SRP023539
 
 cd data/reads/SRP023539
 
 ACCS="../../metadata/SRP023539/accs.txt"
 
-cat $ACCS | parallel -j 1 wget {}
+cat $ACCS | parallel -j $CORES wget {}
 
 bunzip2 *.bz2
